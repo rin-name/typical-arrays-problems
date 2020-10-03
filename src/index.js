f67179ebfda5c83d327cee48af71a27703eb36d6
rin-name <marinka_kostyuk@inbox.ru>
2020-10-03 13:48:59 +0300

    feat: the solution of the problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0) return 0;
+    let res = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (res > array[i]) {
+            res = array[i];
+        }
+    }
+    return res;
 }
 
 exports.max = function max (array) {
+    if (array === undefined || array.length === 0)
   return 0;
+    let res = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (res < array[i]) {
+            res = array[i];
+        }
+    }
+    return res;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0)
+        return 0;
+    let res = 0;
+    for (let i = 0; i < array.length; i++) {
+        res = res + array[i];
+    }
+    return res / array.length;
 }
